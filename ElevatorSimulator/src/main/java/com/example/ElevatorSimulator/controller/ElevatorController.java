afe16d775b7c0689865f6ae5d3c715c6171827a3
daria-medeleanu <medeleanudaria14@gmail.com>
2024-06-03 14:59:03 +0300

    commit

--- ElevatorSimulator/src/main/java/com/example/ElevatorSimulator/controller/ElevatorController.java
+++ ElevatorSimulator/src/main/java/com/example/ElevatorSimulator/controller/ElevatorController.java
@@ -2,6 +2,7 @@ package com.example.ElevatorSimulator.controller;
 
 import com.example.ElevatorSimulator.Simulator;
 import com.example.ElevatorSimulator.entities.Elevator;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ElevatorController {
     private List<Elevator> elevators;
 
---
+++ elevator-simulator-front/src/components/ElevatorSimulator.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function ElevatorSimulator() {
+  const [elevators, setElevators] = useState([]);
+
+  useEffect(() => {
+    const fetchElevators = async () => {
+      try {
+        const response = await axios.get('http://localhost:8082/elevators');
+        setElevators(response.data);
+      } catch (error) {
+        console.error('Error fetching elevator data:', error);
+      }
+    };
+
+    fetchElevators();
+    const intervalId = setInterval(fetchElevators, 1000); // Adjust the interval as needed (e.g., every 1 second)
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+  
+  }, []);
+
+  return (
+    <div>
+      <h1>Elevator Simulator</h1>
+      <ul>
+        {elevators.map((elevator) => (
+          <li key={elevator.name}>{elevator.name} is at floor {elevator.currentFloor}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
